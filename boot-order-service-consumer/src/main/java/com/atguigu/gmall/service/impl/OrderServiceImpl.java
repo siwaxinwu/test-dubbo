8da028ce6ab7f7ee57302ceb771a75ba03700e82
siwaxinwu5 <siwaxinwu5@zto.com>
2020-11-14 20:38:04 +0800

    首次提交

---
+++ boot-order-service-consumer/src/main/java/com/atguigu/gmall/service/impl/OrderServiceImpl.java
@@ -0,0 +1,49 @@
+package com.atguigu.gmall.service.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.service.OrderService;
+import com.atguigu.gmall.service.UserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+/**
+ * 1、将服务提供者注册到注册中心（暴露服务）
+ * 		1）、导入dubbo依赖（2.6.2）\操作zookeeper的客户端(curator)
+ * 		2）、配置服务提供者
+ * 
+ * 2、让服务消费者去注册中心订阅服务提供者的服务地址
+ * @author lfy
+ *roundrobin:负载轮训
+ * random：按照群重随机
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+
+	//@Autowired
+	@Reference(loadbalance="roundrobin", timeout = 1000)
+	//@Reference(url = "127.0.0.1:20882")//dubbo直连
+	UserService userService;
+	
+	@HystrixCommand(fallbackMethod="hello")
+	@Override
+	public List<UserAddress> initOrder(String userId) {
+		// TODO Auto-generated method stub
+		System.out.println("用户id："+userId);
+		//1、查询用户的收货地址
+		List<UserAddress> addressList = userService.getUserAddressList(userId);
+		return addressList;
+	}
+	
+	
+	public List<UserAddress> hello(String userId) {
+		return Arrays.asList(new UserAddress(10, "测试地址", "1", "测试", "测试", "Y"));
+	}
+	
+
+}
